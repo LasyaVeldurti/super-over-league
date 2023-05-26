78972a33ded5754d3f63aef284deec987d48a387
LasyaVeldurti <lasyasreeveldurti18@gmail.com>
2023-05-26 16:50:57 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+
+  <div className="super-league-container">
+    <h1 className="super-league-heading">Super Over League</h1>
+    <div className="image-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="team-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="team-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
